2db41d950e97e7f6f64c5576faf947e274da8d3d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-02-18 21:53:38 +0100

    Commited by accident

--- Examples/ConvertTo-DSC.ps1
+++
@@ -1,7 +0,0 @@
-﻿Clear-Host
-Import-Module .\DSCParser.psd1 -Force
-
-$ProgressPreference = 'SilentlyContinue'
-
-$DSCObjects = ConvertTo-DSCObject -Path $PSScriptRoot\Test.ps1 -IncludeComments $true
-$DSCObjects | Format-Table
