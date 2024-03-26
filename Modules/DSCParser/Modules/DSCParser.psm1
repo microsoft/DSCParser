6565ce7abcae6e3a3432344527707045c7c11b1d
Nik Charlebois <nicharl@microsoft.com>
2024-03-26 08:52:14 -0400

    Fix for variable

--- Modules/DSCParser/Modules/DSCParser.psm1
+++ Modules/DSCParser/Modules/DSCParser.psm1
@@ -442,7 +442,8 @@ function ConvertTo-DSCObject
                 # If the current property is not a CIMInstance
                 if (-not $valueType.StartsWith('[MSFT_') -and `
                     $valueType -ne '[string]' -and `
-                    $valueType -ne '[string[]]')
+                    $valueType -ne '[string[]]' -and `
+                    -not $isVariable)
                 {
                     # Try to parse the value based on the retrieved type.
                     $scriptBlock = @"
